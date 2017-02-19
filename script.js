3e64023d83a324244f6d8926a1f71a31ee359334
Haley Zeng <hzeng@stuy.edu>
2017-02-19 00:03:11 -0500

    info

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+var info_button = document.getElementById("infoBtn");
+var info = document.getElementById("info");
+var infoShowing = false;
+
+var showHideInfo = function(){
+    if (infoShowing){
+	info.style.display = "none";
+	info_button.innerHTML = "What is this?"
+    }
+    else {
+	info.style.display = "inline";
+	info.style.width = "50%";
+	info_button.innerHTML = "got it"
+    }
+    infoShowing = ! infoShowing;
+}
+
+info_button.addEventListener("click", showHideInfo);
+
+
+
+
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+
+
 
 var size_dropdown = document.getElementById("dropdown");
 var clear_button = document.getElementById("clear");
